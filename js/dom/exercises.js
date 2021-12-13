2e57f59c324c75c530a876ab80ca7a8241442476
Damian Molinski <tedeer@gmail.com>
2021-12-13 22:11:51 +0100

    Start exercise 2

--- js/dom/exercises.js
+++ js/dom/exercises.js
@@ -1,16 +1,19 @@
-const list = document.createElement("ul");
-document.body.appendChild(list);
+// exercise 1
+// const list = document.createElement("ul");
+// document.body.appendChild(list);
 
-for (let i = 0; i < 10; i++) {
-    const li = document.createElement("li");
-    li.textContent = i + 1;
-    list.appendChild(li);
-}
+// for (let i = 0; i < 10; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = i + 1;
+//     list.appendChild(li);
+// }
 
-const last = list.querySelector("li:last-child");
+// const last = list.querySelector("li:last-child");
 
-last.style.color = "red";
-last.textContent = "Jestem ostatnim elementem,";
-last.style.backgroundColor = "blue";
-last.style.padding = "20px";
-last.style.fontSize = "48px";
+// last.style.color = "red";
+// last.textContent = "Jestem ostatnim elementem,";
+// last.style.backgroundColor = "blue";
+// last.style.padding = "20px";
+// last.style.fontSize = "48px";
+
+// exercise 2
